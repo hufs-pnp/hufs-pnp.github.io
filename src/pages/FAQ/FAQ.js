f37c17ef709c1de066cd4bd3523618453d57ee26
김주원 <kjwt1124@icloud.com>
2022-02-20 17:54:39 +0900

    Upload Folder

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -1,7 +1,7 @@
 import React , { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 import ParticlesBg from 'particles-bg';
 import Fade from 'react-reveal/Fade';
 
