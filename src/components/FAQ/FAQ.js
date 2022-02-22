d0e012c0967b4770a93dd23a4f85cf59c0333615
김주원 <kjwt1124@icloud.com>
2022-02-22 10:46:41 +0900

    Change Error

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ParticlesBg from 'particles-bg';
+
 /*
 <div className="containerpy-6">
                 <Link to="/FAQ">
@@ -30,7 +31,7 @@ const FAQ = () => {
     return (
         <section className="text-gray-800 body-font relative">
             <ParticlesBg type="cobweb" bg={true} />
-            <div className="container px-5 pt-16 mx-auto flex">
+            <div className="container px-5 p-16 mx-auto flex">
                 <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 border border-gray-200 shadow-md">
                     <h2 className="text-indigo-600 text-2xl mb-1 font-medium title-font">
                         Asked Question
@@ -38,10 +39,11 @@ const FAQ = () => {
                     <p className="leading-relaxed mt-2 mb-5 text-gray-600">
                         PNP FAQ 게시판입니다. 궁금하신 내용을 검색해주세요~
                     </p>
-                
-                <button className="text-white bg-indigo-500 border-0 w-full mt-5 py-2 px-20 focus:outline-none hover:bg-indigo-600 rounded test-xs lg:text-base animate-bounce mt-2">
-                    FAQ 게시판 바로가기
-                </button>
+                    <Link to="/FAQ">
+                    <button className="text-white bg-indigo-500 border-0 w-full mt-5 py-2 px-20 focus:outline-none hover:bg-indigo-600 rounded test-xs lg:text-base animate-bounce mt-2">
+                        FAQ 게시판 바로가기
+                    </button>
+                    </Link>
                 <p className="text-xs text-gray-500 mt-3">
                         직접 운영진에 질문하시려면 아래의 카카오톡 플러스칱구를 이용해 질문해주세요~
                     </p>
