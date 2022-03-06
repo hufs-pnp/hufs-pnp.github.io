9d86f59b3a0c644f98dc8d4f9b263e62030d3a37
김주원 <kjwt1124@icloud.com>
2022-03-06 13:31:51 +0900

    Upload footer developer names

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,18 @@ const Footer = () => {
     return (
         <footer className="text-gray-700 body-font">
             <div className="bg-gray-200">
-                <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                <div className="container mx-auto pt-4 px-5 flex flex-wrap flex-col sm:flex-row">
                     <p className="text-gray-500 text-sm text-center sm:text-left">
                         © {currentYear} PnP
                     </p>
                     <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">
                         경기도 용인시 처인구 모현읍 외대로 81, 한국외국어대학교
                         글로벌캠퍼스 공과대학 316호
+                    </span>
+                </div>
+                <div className="container mx-auto pb-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                    <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">
+                        Developed by 김대원, 김시현, 김주원, 김태우, 이지석, 허성일
                     </span>
                 </div>
             </div>
