a55d6c32f2ab955b2b9d704917ecc83e1c484bd1
SeongilHeo <tjddlf101@gmail.com>
2022-03-29 23:31:20 +0900

    Apply Google Analytics GA4

--- src/components/RouteChangeTracker.js
+++ src/components/RouteChangeTracker.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
+import ReactGA4 from 'react-ga4';
+import gtag from 'react-ga4/src/gtag';
 const RouteChangeTracker = ({ history }) => {
 
     history.listen((location, action) => {
-        ReactGA.set({ page: location.pathname });
         ReactGA.pageview(location.pathname);
+        // const ref_link= location.href.includes("hufspnp.com") ? "(self)" : location.href;
+        ReactGA4.send({hitType: "pageview", path: location.pathname, location: location.pathname, title: location.pathname}); //, page_referrer: "(self)"});
     });
 
     return <div></div>;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
+import ReactGA4 from 'react-ga4';
 
 ReactGA.initialize("UA-190309559-2");
+ReactGA4.initialize("G-5KE45ZFDM1");
 
 ReactDOM.render(
   <React.StrictMode>
