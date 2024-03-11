091c8a10d0b100dd78a7e04c75ebc3d7d2e08247
24ya <bsjp2012@hufs.ac.kr>
2024-03-11 17:24:47 +0900

    Member

--- src/components/Member/MemberList.js
+++ src/components/Member/MemberList.js
@@ -2,600 +2,180 @@ import seongjaerokprofile from '../../static/images/profile/seongjaerok.png';
 
 const members = [
     {
-		name: "배준성",
-		classOf: "15학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "김명수",
-		classOf: "17학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "허성일",
-		classOf: "16학번",
-		blogURL: "https://heo-seongil.tistory.com",
-		profileImage: ''
-	},
-	{
-		name: "전승현",
-		classOf: "17학번",
-		blogURL: "https://jay-jeon.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "원동현",
-		classOf: "17학번",
-		blogURL: "https://hellol77.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "김현주",
-		classOf: "17학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "김태우",
-		classOf: "17학번",
-		blogURL: "https://velog.io/@kimhaech",
-		profileImage: ''
-	},
-	{
-		name: "황서정",
-		classOf: "19학번",
-		blogURL: "https://6dreamhigh.tistory.com",
-		profileImage: ''
-	},
-	{
-		name: "이수현",
-		classOf: "19학번",
-		blogURL: "https://bit.ly/3xARQSo",
-		profileImage: ''
-	},
-	{
-		name: "오영근",
-		classOf: "19학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "배문혁",
-		classOf: "19학번",
-		blogURL: "https://blog.naver.com/dobok11",
-		profileImage: ''
-	},
-	{
-		name: "박소연",
-		classOf: "19학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이혜영",
-		classOf: "19학번",
-		blogURL: "https://blog.naver.com/hyeylee",
-		profileImage: ''
-	},
-	{
-		name: "이지석",
-		classOf: "19학번",
-		blogURL: "https://dev-jiseok.github.io/",
-		profileImage: ''
-	},
-	{
-		name: "손성경",
-		classOf: "18학번",
-		blogURL: "https://velog.io/@shonsk0220",
-		profileImage: ''
-	},
-	{
-		name: "김대엽",
-		classOf: "18학번",
-		blogURL: "https://blog.naver.com/kimdyk1",
-		profileImage: ''
-	},
-	{
-		name: "이현진",
-		classOf: "18학번",
-		blogURL: "https://blog.naver.com/qhdekfdl6955",
-		profileImage: ''
-	},
-	{
-		name: "이정민",
-		classOf: "20학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "최준혁",
-		classOf: "20학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이서영",
-		classOf: "20학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "임지선",
-		classOf: "20학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이주윤",
-		classOf: "20학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "유승리",
-		classOf: "20학번",
-		blogURL: "https://velog.io/@uvictoli",
-		profileImage: ''
-	},
-	{
-		name: "성재록",
-		classOf: "20학번",
-		blogURL: "https://m.blog.naver.com/PostList.naver?blogId=sjr_team, sungjaerok.blogspot.com",
-		profileImage: ''
-	},
-	{
-		name: "나리나",
-		classOf: "20학번",
-		blogURL: "https://www.blogger.com/blog/posts/3508692332239462121?hl=ko&tab=jj",
-		profileImage: ''
-	},
-	{
-		name: "김송하",
-		classOf: "20학번",
-		blogURL: "https://velog.io/@songhaaa",
-		profileImage: ''
-	},
-	{
-		name: "김득호",
-		classOf: "20학번",
-		blogURL: "https://ho97.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "김대원",
-		classOf: "20학번",
-		blogURL: "https://dae-won.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "구하민",
-		classOf: "20학번",
-		blogURL: "https://hamin2065.github.io/",
-		profileImage: ''
-	},
-	{
-		name: "송종빈",
-		classOf: "20학번",
-		blogURL: "https://velog.io/@jongbeensong",
-		profileImage: ''
-	},
-	{
-		name: "백건우",
-		classOf: "20학번",
-		blogURL: "https://velog.io/@phlox3959",
-		profileImage: ''
-	},
-	{
-		name: "박용훈",
-		classOf: "19학번",
-		blogURL: "https://velog.io/@yonpark",
-		profileImage: ''
-	},
-	{
-		name: "안진혁",
-		classOf: "18학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "황찬일",
-		classOf: "18학번",
-		blogURL: "https://velog.io/@illli_705",
-		profileImage: ''
-	},
-	{
-		name: "조권휘",
-		classOf: "18학번",
-		blogURL: "https://velog.io/@ckh0824",
-		profileImage: ''
-	},
-	{
-		name: "이승민",
-		classOf: "18학번",
-		"github": "https://github.com/seungmin8606",
-		profileImage: ''
-	},
-	{
-		name: "김주영",
-		classOf: "18학번",
-		blogURL: "https://velog.io/@rozi0901",
-		profileImage: ''
-	},
-	{
-		name: "김석윤",
-		classOf: "16학번",
-		blogURL: "https://velog.io/@kjkksu2",
-		profileImage: ''
-	},
-	{
-		name: "박세현",
-		classOf: "21학번",
-		blogURL: "https://sarah1918.tistory.com",
-		profileImage: ''
-	},
-	{
-		name: "이서화",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "조윤주",
-		classOf: "21학번",
-		blogURL: "https://yjyjy99.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "이수경",
-		classOf: "21학번",
-		blogURL: "https://bit.ly/3w0sazA",
-		profileImage: ''
-	},
-	{
-		name: "안혜련",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "송가현",
-		classOf: "21학번",
-		blogURL: "https://github.com/SongGaHyeon",
-		profileImage: ''
-	},
-	{
-		name: "박서원",
-		classOf: "21학번",
-		blogURL: "tjdnjs.github.io",
-		profileImage: ''
-	},
-	{
-		name: "김태우",
-		classOf: "21학번",
-		blogURL: "https://url.kr/4yzcbj",
-		profileImage: ''
-	},
-	{
-		name: "김지호",
-		classOf: "21학번",
-		blogURL: "https://jiho001.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "강효빈",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "강도훈",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이채원",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "오세창",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이성준",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이사야",
-		classOf: "20학번",
-		blogURL: "https://d-0-0-b.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "김태호",
-		classOf: "20학번",
-		blogURL: "https://blog.naver.com/ben108",
-		profileImage: ''
-	},
-	{
-		name: "황병훈",
-		classOf: "21학번",
-		blogURL: "https://blog.naver.com/h1009218",
-		profileImage: ''
-	},
-	{
-		name: "정보미",
-		classOf: "21학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "임준혁",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "박의민",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "노유신",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "김승주",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "신예원",
-		classOf: "22학번",
-		blogURL: "https://blog.naver.com/rac8793",
-		profileImage: ''
-	},
-	{
-		name: "정종원",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "허태수",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이진명",
-		classOf: "22학번",
-		blogURL: "https://jinlight-tech.tistory.com/",
-		profileImage: ''
-	},
-	{
-		name: "송경환",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "서정훈",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "박지훈",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "박세현",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "박우승",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "김시현",
-		classOf: "21학번",
-		blogURL: "https://k4sud0n.tistory.com",
-		profileImage: ''
-	},
-	{
-		name: "임초현",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "심규빈",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "김민준",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "전세은",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이상연",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "태유빈",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "차정민",
-		classOf: "22학번",
-		blogURL: '',
-		profileImage: ''
-	},
-	{
-		name: "이건우",
-		classOf: "22학번",
-		blogURL: '',
-
-		profileImage: ''
-	},
-    {
-        name: '조권휘',
-        classOf: '18학번',
-        profileImage: '',
-        blogURL: 'https://velog.io/@ckh0824',
+        name: "이사야",
+        classOf: "20학번",
+        blogURL: "",
+        profileImage: ''
     },
     {
-        name: '백건우',
-        classOf: '20학번',
-        profileImage: '',
-        blogURL: 'https://velog.io/@phlox3959',
-    },
-    {
-        name: '김송하',
-        classOf: '20학번',
-        profileImage: '',
+        name: "김대현",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '성재록',
-        classOf: '20학번',
-        profileImage: seongjaerokprofile,
-        blogURL: 'https://sungjaerok.blogspot.com/',
-    },
-    {
-        name: '유승리',
-        classOf: '20학번',
-        profileImage: '',
+        name: "이정민",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '김태호',
-        classOf: '20학번',
-        profileImage: '',
-        blogURL: 'https://blog.naver.com/ben108',
-    },
-    {
-        name: '김대원',
-        classOf: '20학번',
-        profileImage: '',
+        name: "최준혁",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '강도훈',
-        classOf: '21학번',
-        profileImage: '',
+        name: "임지선",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '강효빈',
-        classOf: '21학번',
-        profileImage: '',
+        name: "성재록",
+        classOf: "20학번",
+        blogURL: "https://m.blog.naver.com/PostList.naver?blogId=sjr_team, sungjaerok.blogspot.com",
+        profileImage: ''
+    },
+    {
+        name: "김대원",
+        classOf: "20학번",
+        blogURL: "https://dae-won.tistory.com/",
+        profileImage: ''
+    },
+    {
+        name: "송은지",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '송가현',
-        classOf: '21학번',
-        profileImage: '',
+        name: "박인기",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '안혜련',
-        classOf: '21학번',
-        profileImage: '',
+        name: "송강규",
+        classOf: "20학번",
+        blogURL: "https://ggsong0328.github.io/",
+        profileImage: ''
+    },
+    {
+        name: "조수영",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '이수경',
-        classOf: '21학번',
-        profileImage: '',
+        name: "정민규",
+        classOf: "20학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '조윤주',
-        classOf: '21학번',
-        profileImage: '',
+        name: "조윤주",
+        classOf: "21학번",
+        blogURL: "https://yjyjy99.tistory.com/",
+        profileImage: ''
+    },
+    {
+        name: "임다훈",
+        classOf: "21학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '임도훈',
-        classOf: '19학번',
-        profileImage: '',
+        name: "정보미",
+        classOf: "21학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '최겸',
-        classOf: '19학번',
-        profileImage: '',
+        name: "안혜련",
+        classOf: "21학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '김대현',
-        classOf: '20학번',
-        profileImage: '',
+        name: "송가현",
+        classOf: "21학번",
+        blogURL: "https://github.com/SongGaHyeon",
+        profileImage: ''
+    },
+    {
+        name: "강도훈",
+        classOf: "21학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '박세현',
-        classOf: '20학번',
-        profileImage: '',
-        blogURL: 'https://blog.naver.com/sese2204',
-    },
-    {
-        name: '정민규',
-        classOf: '20학번',
-        profileImage: '',
+        name: "이채원",
+        classOf: "21학번",
         blogURL: '',
+        profileImage: ''
     },
     {
-        name: '조승현',
-        classOf: '20학번',
-        profileImage: '',
-        blogURL: 'https://seunghyuncho22.github.io/',
+        name: "신예원",
+        classOf: "22학번",
+        blogURL: "https://blog.naver.com/rac8793",
+        profileImage: ''
     },
     {
-        name: '김시현',
-        classOf: '21학번',
-        profileImage: '',
-        blogURL: 'https://k4sud0n.tistory.com/',
+        name: "김시현",
+        classOf: "21학번",
+        blogURL: "https://k4sud0n.tistory.com",
+        profileImage: ''
     },
     {
-        name: '임다훈',
-        classOf: '21학번',
-        profileImage: '',
+        name: "장태현",
+        classOf: "21학번",
         blogURL: '',
+        profileImage: ''
     },
+    {
+        name: "전세은",
+        classOf: "22학번",
+        blogURL: '',
+        profileImage: ''
+    },
+    {
+        name: "임초현",
+        classOf: "22학번",
+        blogURL: '',
+        profileImage: ''
+    },
+    {
+        name: "김소정",
+        classOf: "22학번",
+        blogURL: '',
+        profileImage: ''
+    },
+    {
+        name: "김하원",
+        classOf: "23학번",
+        blogURL: '',
+        profileImage: ''
+    },
+    {
+        name: "백민서",
+        classOf: "23학번",
+        blogURL: '',
+        profileImage: ''
+    },
+    {
+        name: "이소은",
+        classOf: "23학번",
+        blogURL: '',
+        profileImage: ''
+    },
+    {
+        name: "이동욱",
+        classOf: '',
+        blogURL: '',
+        profileImage: ''
+    }
 ];
+
 
 export default members;
